bc5107c6741de57e19145ee2f14a152959751312
Nelson U <urbinans@gmail.com>
2018-01-11 14:26:37 -0400

    Iniciando el proyecto

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// hello.cpp: Nelson Urbina
+// Description: A program that prints "Hello World!"
+
+#include <iostream>
+// using namespace std;
+
+int main(){
+  std::cout << "Hello World!" << '\n';
+  return 0;
+}
