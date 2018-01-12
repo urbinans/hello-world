11435b9696f2839bdf3d68aa8c0658543c215e47
Nelson U <urbinans@gmail.com>
2018-01-12 09:49:16 -0400

    'Creando una piramide con substr()'

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,23 @@
 // hello.cpp: Nelson Urbina
 // Description: A program that prints "Hello World!"
+// Now, let's play a little with it
 
 #include <iostream>
-// using namespace std;
+#include <string>
 
 int main(){
   std::cout << "Hello World!" << '\n';
+  /** 
+   * This idea came out from a post I read in the Q&A section of Sololearn:
+   *  Let's make a piramid!
+   **/
+  std::string s = "Hello World!";
+  for (int i = s.length(); i >= 0;--i){
+    std::cout << s.substr(0,s.length()-i) << '\n';
+  }
+  // And the other way around
+  for (int i = 0; i < s.length();++i){
+    std::cout << s.substr(0,s.length()-i) << '\n';
+  }
   return 0;
 }
