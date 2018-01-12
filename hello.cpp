808aacc476fe7c0149b8bec185658a3c815300c6
Nelson U <urbinans@gmail.com>
2018-01-12 10:33:05 -0400

    'Testing some io functions'

--- hello.cpp
+++ hello.cpp
@@ -12,12 +12,25 @@ int main(){
    *  Let's make a piramid!
    **/
   std::string s = "Hello World!";
-  for (int i = s.length(); i >= 0;--i){
+  for (int i = s.length();i >= 0;--i){
     std::cout << s.substr(0,s.length()-i) << '\n';
   }
   // And the other way around
-  for (int i = 0; i < s.length();++i){
+  for (int i = 0;i < s.length();++i){
     std::cout << s.substr(0,s.length()-i) << '\n';
   }
+  
+  std::cout.width(20);
+  std::cout.fill('=');
+  std::cout << '\n';
+
+  std::cout.width(17);
+  for (int i = 0;i < 6;++i){
+    for (int j = 0;j < 4; ++j){
+      std::cout << s;
+    }
+    std::cout << '\n';
+  }
+
   return 0;
 }
